53d5d05f104a3a4ec3af50696d01a3feb0afe83a
Gelvazio <gelvazio@t2.dev.br>
2024-08-22 21:07:17 -0300

    ok

--- api/core/header.php
+++ api/core/header.php
@@ -3,10 +3,14 @@
 $request_uri = $_SERVER['REQUEST_URI'];
 $url_inicial = "";
 $url_inicial_css = "../";
-if($request_uri == "/sistemaescolar/api/index.php"){
-    $url_inicial_css = "api/";
+if($request_uri == "/sistemaescolar/api/index.php" || $request_uri == "/sistemaescolar/api/"){
+    $url_inicial_css = "http://localhost/sistemaescolar/api/";
     $url_inicial = "api/";
 }
+
+//echo 'url_inicial_css: ' . $url_inicial_css . '<br>';
+//echo 'request_uri: ' . $request_uri . '<br>';
+//echo 'link css completo: ' . $url_inicial_css . 'css/style.css <br>';
 
 echo '
 <!DOCTYPE html>
@@ -25,6 +29,7 @@ echo '
             <li><a href="../' . $url_inicial . 'aluno/consulta_aluno.php">Alunos</a></li>
             <li><a href="../' . $url_inicial . 'professor/consulta_professor.php">Professor</a></li>
             <li><a href="../' . $url_inicial . 'escola/consulta_escola.php">Escola</a></li>
+            <li><a href="../' . $url_inicial . 'geradorboletim/index.html">Boletim Escolar</a></li>
         </ul>
         <hr>
     </div>
--- api/index.php
+++ api/index.php
@@ -2,11 +2,6 @@
 
 require_once("core/header.php");
 
-// echo '<link rel="stylesheet" href="css/style.css">';
-// echo '<link rel="stylesheet" href="css/button.css">';
-// echo '<link rel="stylesheet" href="css/header.css">';
-
-
 require_once("core/sistema.php");
 
 require_once("core/footer.php");
